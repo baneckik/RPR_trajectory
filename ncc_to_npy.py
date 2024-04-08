6a1b46d6b265eb8b3495d1c48284d726a4503878
Krzysztof Banecki <kbanecki@k165.mini.pw.edu.pl>
2024-04-08 13:59:04 +0200

    separate function for folder processing

--- ncc_to_npy.py
+++ ncc_to_npy.py
@@ -11,12 +11,12 @@ def ncc_to_npy(path, chrom, size=100, binary=True):
     df.columns = ["chr1", "pos1", "chr2", "pos2"]
     df = df[(df.chr1 == chrom) & (df.chr2 == chrom)]
 
-    pos_min = np.min(list(df.pos1)+list(df.pos2))
+    pos_min = np.min(list(df.pos1) + list(df.pos2))
     pos_max = np.max(list(df.pos1) + list(df.pos2))
 
     array = np.zeros((size, size))
     for i in range(df.shape[0]):
-        pos1_i = int((df.pos1.iloc[i]-pos_min)/(pos_max-pos_min+1)*size)
+        pos1_i = int((df.pos1.iloc[i] - pos_min) / (pos_max - pos_min + 1) * size)
         pos2_i = int((df.pos2.iloc[i] - pos_min) / (pos_max - pos_min + 1) * size)
         array[pos1_i, pos2_i] += 1
         array[pos2_i, pos1_i] += 1
@@ -27,21 +27,24 @@ def ncc_to_npy(path, chrom, size=100, binary=True):
         return array
 
 
+def ncc_folder_to_npy(input_folder, output_folder, chrom, size, png_folder=None):
+    files = os.listdir(input_folder)
+    files = [file for file in files if file.endswith(".ncc")]
+    for file in files:
+        file_path = os.path.join(input_folder, file)
+        path_out = os.path.join(output_folder, file[:-4] + "_{}.npy".format(chrom))
+        hic_array = ncc_to_npy(file_path, chrom, size, binary=False)
+        if png_folder is not None:
+            matrix_plot(hic_array, os.path.join(png_folder, "schic.png"))
+        np.save(path_out, hic_array)
+
+
 if __name__ == "__main__":
     main_path = "./examples/one_patski"
-    chrom = "chr7-M"
-    size = 200
+    chromosome = "chr7-M"
+    matrix_size = 200
 
     ncc_path = main_path + "/ncc"
     npy_path = main_path + "/npy"
 
-    files = os.listdir(ncc_path)
-    files = [file for file in files if file.endswith(".ncc")]
-    for file in files:
-        file_path = os.path.join(ncc_path, file)
-        path_out = os.path.join(npy_path, file[:-4] + "_{}.npy".format(chrom))
-        hic_array = ncc_to_npy(file_path, chrom, size, binary=False)
-        matrix_plot(hic_array, os.path.join(main_path, "test_schic.png"))
-        np.save(path_out, hic_array)
-
-
+    ncc_folder_to_npy(ncc_path, npy_path, chromosome, matrix_size, main_path)
